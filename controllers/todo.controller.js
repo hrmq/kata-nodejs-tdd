70418f151224f417f4fab113acf814a249559864
hrmq <h.rahimi.moqadam@gmail.com>
2020-05-01 14:54:04 -0400

    add test for getting json body after creating a todo

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,6 +1,6 @@
 const TodoModel = require('../models/todo.model');
 
 exports.createTodo = function (req, res, next) {
-  TodoModel.create(req.body);
-  res.status(201).send();
+  const todoModel = TodoModel.create(req.body);
+  res.status(201).json(todoModel);
 };
