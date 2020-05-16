dcc83cea3d81ee58bc54dd98371e3adc099b5fb3
hrmq <h.rahimi.moqadam@gmail.com>
2020-05-15 21:06:50 -0400

    write unit test for error handling

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,6 +1,11 @@
 const TodoModel = require('../models/todo.model');
 
 exports.createTodo = async function (req, res, next) {
-  const todoModel = await TodoModel.create(req.body);
-  res.status(201).json(todoModel);
+  try {
+    const todoModel = await TodoModel.create(req.body);
+    res.status(201).json(todoModel);
+  } catch (err) {
+    console.log(err, 'djsaljld');
+    next(err.message);
+  }
 };
