c0271322f256c7a7beeb9015ff9b3ed51975a0a4
hrmq <h.rahimi.moqadam@gmail.com>
2020-05-22 13:40:32 -0400

    add integration test for getting all todos

--- tests/integration/todo.controller.int.test.js
+++ tests/integration/todo.controller.int.test.js
@@ -1,10 +1,20 @@
 const request = require('supertest');
 const app = require('../../app');
+
 const newTodo = require('../mock-data/new-todo.json');
+const allTodos = require('../mock-data/all-todos.json');
 
 const endpointUrl = '/todos/';
 
 describe(endpointUrl, () => {
+  it('GET ' + endpointUrl, async () => {
+    const response = await request(app).get(endpointUrl);
+    expect(response.statusCode).toBe(200);
+    expect(typeof response.body).toBe('object');
+    expect(response.body[0].title).toBeDefined();
+    expect(response.body[0].done).toBeDefined();
+  });
+
   it('POST ' + endpointUrl, async () => {
     const response = await request(app).post(endpointUrl).send(newTodo);
     expect(response.statusCode).toBe(201);
